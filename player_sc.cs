c8d55466affda00b58a20d131ae9e78fc44b0047
AsmBrk <03asmbrki1907@gmail.com>
2025-10-14 00:15:06 +0300

    Add files via upload

---
+++ player_sc.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class player_sc : MonoBehaviour
+{
+//public float speed = 3 ;
+//private float speed = 3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      //  transform.position = new Vector3(10,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  transform.position = new Vector3(5f,0,0*speed*Time.deltaTime);
+    }
+}
